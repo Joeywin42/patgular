566ec6bbd785ce8feba9b59721962cdf8930349d
Joeywin42 <joeywin42@gmail.com>
2024-01-05 00:18:38 -0500

    project

---
+++ src/main/java/com/angles/patgular/PatgularApplication.java
@@ -0,0 +1,13 @@
+package com.angles.patgular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatgularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatgularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/angles/patgular/PatgularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angles.patgular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatgularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
